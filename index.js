26f9f5642686535aba3519cbfbb15474f3b38f95
Rafael Ferreira Lemes <rafaelf.lemes@gmail.com>
2023-10-19 13:47:53 -0300

    feat:adicionado cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var bodyParser = require('body-parser')
 var express = require("express")
 var app = express()
 var router = require("./routes/routes")
+var cors = require('cors')
  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+app.use(cors());
 
 app.use("/",router);
 
